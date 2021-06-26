7619d735361771b4d4a63c2702975f0ded00b5c0
nadjagv <gvozdenacn@gmail.com>
2021-06-26 13:21:26 +0200

    Revert "deleted unnecessary files"
    This reverts commit 41c6cda5b7835989219633d19f062b94a29b6b04.

---
+++ src/test/java/com/nadjagv/nadjagvozdenaccode92021tennisservice/NadjaGvozdenacCode92021TennisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadjagv.nadjagvozdenaccode92021tennisservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NadjaGvozdenacCode92021TennisServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
